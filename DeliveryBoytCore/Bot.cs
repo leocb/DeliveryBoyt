ad1aff2ff3ab02a95acbe9f2c899d90427eacbd9
leocb <leobottaro@gmail.com>
2021-09-29 00:26:11 -0300

    Base Discord bot

---
+++ DeliveryBoytCore/Bot.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace DeliveryBoyt.Core
+{
+    public static class Bot
+    {
+        public static string Token { set; private get; }
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private static DiscordSocketClient _client;
+        public static async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, Token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+    }
+}
---
+++ DeliveryBoytDisplayServer/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using DeliveryBoyt.Core;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DeliveryBoyt.DisplayServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Bot.Token = Properties.Settings.Default.BotToken;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.BotToken = TokenTextBox.Text;
+            Properties.Settings.Default.Save();
+            Bot.Token = Properties.Settings.Default.BotToken;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _ = Bot.MainAsync();
+        }
+    }
+}
